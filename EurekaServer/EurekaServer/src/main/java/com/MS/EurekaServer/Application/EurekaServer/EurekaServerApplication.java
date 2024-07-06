fe4959984c71d0e61add20e4d71a0f0d7a9b037f
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-07-06 18:37:53 -0300

    Commit with Eureka Server configuration

---
+++ EurekaServer/EurekaServer/src/main/java/com/MS/EurekaServer/Application/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.MS.EurekaServer.Application.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
